a4bfc795d11b19f718d3f7f55e5529b1beb49c7b
💰 <julien@Air-de-Julien.lan>
2026-09-13 11:04:25 +0200

    fix lang fr + disable translate safari

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,8 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="fr"
+      translate="no"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}
